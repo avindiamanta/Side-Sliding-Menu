35e9b03a133d8093bf4eaf205c4eef090ba57e9d
Avindiamanta <avinophotography@gmail.com>
2016-02-29 19:06:04 +0700

    Initial Commit

---
+++ Side Sliding Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Side Sliding Menu
+//
+//  Created by Tiffany Diamanta on 2/29/16.
+//  Copyright © 2016 Tiffany Diamanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Side Sliding MenuTests/Side_Sliding_MenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Side_Sliding_MenuTests.swift
+//  Side Sliding MenuTests
+//
+//  Created by Tiffany Diamanta on 2/29/16.
+//  Copyright © 2016 Tiffany Diamanta. All rights reserved.
+//
+
+import XCTest
+@testable import Side_Sliding_Menu
+
+class Side_Sliding_MenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
